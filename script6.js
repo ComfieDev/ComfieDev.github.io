0331be88008b692af58cdf920157af5dbb126502
Glitch (olisinpa) <none>
2025-05-13 20:26:11 +0000

    🌊🛣️ Updated with Glitch

--- script6.js
+++ script6.js
@@ -1,3 +1,3 @@
 window.onload = function () {
-  window.location.href = "https://pizzapranks.com/product/olisinpa-split-by-samuli-pietikainen/";
+  window.location.href = "https://comfiedev.itch.io/olisinpa-split";
 };
